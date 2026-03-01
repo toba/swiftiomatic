0061b60b3c9479b812ce4568d2e709eb65da4fa5
Jason Abbott <jason@toba.app>
2026-03-01 14:57:13 -0700

    reformat three files missed in prior commit (7y2-kma)

--- Sources/Swiftiomatic/Rules/Ordering/Structure/ComputedAccessorsOrderRule+Configuration.swift
+++ Sources/Swiftiomatic/Rules/Ordering/Structure/ComputedAccessorsOrderRule+Configuration.swift
@@ -1,20 +1,20 @@
 struct ComputedAccessorsOrderConfiguration: SeverityBasedRuleOptions {
-  enum Order: String, AcceptableByConfigurationElement {
-    case getSet = "get_set"
-    case setGet = "set_get"
-  }
-
-  @ConfigurationElement(key: "severity")
-  var severityConfiguration = SeverityConfiguration<Parent>(.warning)
-  @ConfigurationElement(key: "order")
-  private(set) var order = Order.getSet
-  typealias Parent = ComputedAccessorsOrderRule
-  mutating func apply(configuration: [String: Any]) throws(SwiftiomaticError) {
-    try applySeverityIfPresent(configuration)
-    if let value = configuration[$order.key] {
-      try order.apply(value, ruleID: Parent.identifier)
+    enum Order: String, AcceptableByConfigurationElement {
+        case getSet = "get_set"
+        case setGet = "set_get"
     }
-    warnAboutUnknownKeys(in: configuration)
-    validate()
-  }
+
+    @ConfigurationElement(key: "severity")
+    var severityConfiguration = SeverityConfiguration<Parent>(.warning)
+    @ConfigurationElement(key: "order")
+    private(set) var order = Order.getSet
+    typealias Parent = ComputedAccessorsOrderRule
+    mutating func apply(configuration: [String: Any]) throws(SwiftiomaticError) {
+        try applySeverityIfPresent(configuration)
+        if let value = configuration[$order.key] {
+            try order.apply(value, ruleID: Parent.identifier)
+        }
+        warnAboutUnknownKeys(in: configuration)
+        validate()
+    }
 }
